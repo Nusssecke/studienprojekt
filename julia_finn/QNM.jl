1ee7c387da08e6617ab0fb6567368b97460ed55d
Nusssecke <finn.jannik.wagner@physik.uni-giessen.de>
2024-04-23 15:29:57 +0200

    optimization

--- julia_finn/QNM.jl
+++ julia_finn/QNM.jl
@@ -1,6 +1,8 @@
 using DifferentialEquations
+using BenchmarkTools
 using BoundaryValueDiffEq
 using Plots
+using NLsolve
 
 include("horizon_expansion.jl")
 
@@ -8,7 +10,7 @@ f(u, qt) = 1 - (1+qt^2) * u^2 + qt^2 * u^3 # blackening_factor
 df(u, qt) = -2 * (1+qt^2) * u + 3 * qt^2 * u^2 # derivative of blackening_factor
 
 
-function phi(du, u, p, t)
+function shear_mode_eq!(du, u, p, t)
     # u is current state variable, du is the derivative of u at time t, t is current time
     c0, qt, omega, kk = p # p is a vector of parameters
 
@@ -17,59 +19,75 @@ function phi(du, u, p, t)
     du[2] = dphiImag
     phi = phiReal + im * phiImag
     dphi = dphiReal + im * dphiImag
-    eq = (-((omega^2 - kk^2*(-1 + t)*(-1 + t*(-1 + qt^2*t)))* phi)/(4*(-1 + t)^2*t*(1 + t - qt^2*t^2)^2) + -((-1 + t*(-1 + qt^2*t))*(-1 + t^2*(-1 + qt^2*(-1 + 2*t)))* dphi)/((-1 + t)*t*(1 + t - qt^2*t^2)^2))* (1 + t - qt^2*t^2)^2/((-1 + t*(-1 + qt^2*t))^2)
+    eq = (
+            -((omega^2 - kk^2*(-1 + t)*(-1 + t*(-1 + qt^2*t)))* phi)/(4*(-1 + t)^2*t*(1 + t - qt^2*t^2)^2) 
+            -((-1 + t*(-1 + qt^2*t))*(-1 + t^2*(-1 + qt^2*(-1 + 2*t)))* dphi)/((-1 + t)*t*(1 + t - qt^2*t^2)^2)
+        ) * (1 + t - qt^2*t^2)^2/((-1 + t*(-1 + qt^2*t))^2)
     du[3] = real(eq)
     du[4] = imag(eq)
+    nothing
 end
 
 # boundary conditions
-function bc1!(residual, u, p, t)
-    c0, qt, omega, kk = p
-    residual[1] = real(phiHorizonExpansion(uHorizonNumerical, c0, qt, omega, kk)) - u[end][1] # solution at horizon should be the horizon expansion
-    residual[2] = imag(phiHorizonExpansion(uHorizonNumerical, c0, qt, omega, kk)) - u[end][2] # solution at horizon should be the horizon expansion
-    residual[3] = real(dphiHorizonExpansion(uHorizonNumerical, c0, qt, omega, kk)) - u[end][3] # derivative at horizon should be the derivative of the horizon expansion
-    residual[4] = imag(dphiHorizonExpansion(uHorizonNumerical, c0, qt, omega, kk)) - u[end][4] # derivative at horizon should be the derivative of the horizon expansion
+function boundary_condition!(residual, u, p, t)
+    residual[1] = real(phiHorizonExpansion(uHorizonNumerical, p...)) - u[end][1] # solution at horizon should be the horizon expansion
+    residual[2] = imag(phiHorizonExpansion(uHorizonNumerical, p...)) - u[end][2] # solution at horizon should be the horizon expansion
+    residual[3] = real(dphiHorizonExpansion(uHorizonNumerical, p...)) - u[end][3] # derivative at horizon should be the derivative of the horizon expansion
+    residual[4] = imag(dphiHorizonExpansion(uHorizonNumerical, p...)) - u[end][4] # derivative at horizon should be the derivative of the horizon expansion
 end
 
-epsilon = 1/1000000000
+epsilon = big(1.0)/big(1000000000.0)
 uBoundaryNumerical = epsilon
-uHorizonNumerical = 1-1/10
+uHorizonNumerical = big(1.0)-big(1.0)/big(10.0)
 
-function phiSol(qt,omega, kk, c0=1)
-    println("Starting calculation of phiSol with qt = ", qt, ", omega = ", omega, ", kk = ", kk)
+function shear_mode(qt, omega, kk, c0=1)
+    # println("Starting calculation of phiSol with qt = ", qt, ", omega = ", omega, ", kk = ", kk)
+
     u0 = [1, 0, 1, 0]; # initial conditions    
     tspan = (uBoundaryNumerical, uHorizonNumerical) # Possible values for u
+    parameters = [c0, qt, omega, kk]
 
-    # c0, qt, omega, kk
-    p = [1, qt, omega, kk] # parameters
-
-    bvp = BVProblem(phi, bc1!, [1/2, 0, 1/2, 0], tspan, p)
-    sol = solve(bvp, Shooting(Vern7()), dtmax = 1/1000, reltol = 1e-12, abstol = 1e-12)
+    boundary_value_problem = BVProblem(shear_mode_eq!, boundary_condition!, u0, tspan, parameters)
+    # dtmax = 1/1000, reltol = 1e-12, abstol = 1e-12
+    solution = solve(boundary_value_problem, MIRK2(), dt= 0.05)
 
     # Plot the first to variables of the solution atop one another
-    plt = plot(sol)
-    display(plt)
-    solution = u -> [sol(u, idxs = 1), sol(u, idxs = 2)]
-    println("Finished calculation of phiSol with qt = ", qt, ", omega = ", omega, ", kk = ", kk)
-    return solution
+    display(plot(solution))
+    # println("Finished calculation of phiSol with qt = ", qt, ", omega = ", omega, ", kk = ", kk)
+
+    return u -> [solution(u, idxs = 1), solution(u, idxs = 2)]
 end
 
 # sol = phiSol(0, 1/2, 1/2)
 # println(sol(uBoundaryNumerical))
 # println(sol(1/2))
+sol = shear_mode(sqrt(2) - 1/100, 0.0 + im * -0.003, 1)
+
+function solve_quasinormal_mode(omega)
+    println("Omega = ", omega)
+    solution =  shear_mode(sqrt(2) - 1/100, omega[1] + im * omega[2], 1)
+    println("Solution = ", solution(uBoundaryNumerical))
+    return solution(uBoundaryNumerical)
+end
 
 # qnmRoutine
-using NLsolve
-function salv(omega)
-    println(omega)
-    sol =  phiSol(sqrt(2), omega[1] + im * omega[2], 1)
-    println("solution=", sol(uBoundaryNumerical))
-    return sol(uBoundaryNumerical)
+function qnmRoutine(initial_guess_omega)
+    s = nlsolve(solve_quasinormal_mode, initial_guess_omega, xtol=1e-30, ftol=1e-30)
+    println(s.zero)
 end
 
-# s = nlsolve(salv, [4.0, -3.0], xtol=1e-30, ftol=1e-30)
-# println(s.zero)
+# qnmRoutine([0.0, -0.01])
 
-for i in 1:100
-    println((phiSol(sqrt(2), i/10 - im * i/10, 1))(uBoundaryNumerical))
+#=
+values = zeros(Complex{Float64}, 10, 10)
+for i in 1:10
+    for j in 1:10
+        println("Omega = ", i/100 - im * j/100)
+        value = (shear_mode(sqrt(2) - 1/100 , i/100 - im * j/100, 1))(uBoundaryNumerical)
+        println("Value = ", value[1], " + im * ", value[2])
+        values[i, j] = value[1] + im * value[2]
+    end
 end
+
+print(values)
+=#
