73e35a7b19ddbffaff813006bf3a0ce5c983c0ea
Nusssecke <finn.jannik.wagner@physik.uni-giessen.de>
2024-05-27 15:10:40 +0200

    Aufräumen

--- QNM/src/unit_tests.jl
+++ QNM/src/unit_tests.jl
@@ -13,3 +13,9 @@ println(phiHorizonExpansion(u, c0, qt, omega, kk))
 println("Test Case 2")
 println(dphiHorizonExpansion(u, c0, qt, omega, kk))
 # Expected Output: 0.00123882900990351218987474 + 0.0344982622722421530720753 im
+
+# Steps to do?
+# Solve differential equations and plot the results more appropriately
+# to better be able to see behavior
+
+# Try different takes on the the root finding. Try scaling or fitting the function near an expected root
